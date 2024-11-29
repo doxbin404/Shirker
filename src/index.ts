72e8d418afb48a76eae8f537ce704213f1e97235
Woopwoop <186307464+WoKissu@users.noreply.github.com>
2024-11-29 22:25:54 +0700

    feat(console): Добавлен форматированный вывод в консоль

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import { Telegraf } from 'telegraf';
-import LocalSession from 'telegraf-session-local';
 import { Command } from './commands/command.class.js';
-import { IConfigService } from 'config/config.interface.js';
+import { IConfigService } from './config/config.interface.js';
 import { ConfigService } from './config/config.service.js';
-import { IClientContext } from 'context/context.interface.js';
+import { IClientContext } from './context/context.interface.js';
 import { fileURLToPath, pathToFileURL } from 'node:url';
-import fs from 'node:fs';
+import { ServiceBox } from './services/boxService.js';
+import LocalSession from 'telegraf-session-local';
+import prisma from './prisma/prisma.js';
+import chalk from 'chalk';
 import path from 'node:path';
+import fs from 'node:fs';
 
+const IBox = new ServiceBox();
+const BoxContents = IBox.createBox();
 class Client {
+	private boxContents = BoxContents;
+
 	client: Telegraf<IClientContext>;
 	commands: Command[] = [];
+
 	constructor(private readonly configService: IConfigService) {
 		this.client = new Telegraf<IClientContext>(this.configService.get('TELEGRAM_BOT_TOKEN'));
 		this.client.use(
@@ -19,14 +27,20 @@ class Client {
 	}
 
 	public async init() {
+		IBox.addItem(this.boxContents, { name: `${chalk.bold.cyan('Telegraf.js')}`, value: 'v4.16.3\n' });
+
 		await this.loadCommands();
+		await this.connectPrisma();
+
 		for (const command of this.commands) {
 			command.execute();
 		}
-		this.client.launch();
+
+		IBox.showBox(this.boxContents, { borderColor: 'white', borderStyle: 'round', dimBorder: true, padding: 1, margin: 1 });
+		await this.client.launch();
 	}
 
-	async loadCommands() {
+	private async loadCommands() {
 		const __filename = fileURLToPath(import.meta.url);
 		const __dirname = path.dirname(__filename);
 
@@ -40,7 +54,19 @@ class Client {
 			const commandClass = Object.values(commandModule)[0];
 			if (typeof commandClass === 'function' && commandClass.prototype instanceof Command) {
 				this.commands.push(new (commandClass as new (client: Telegraf<IClientContext>) => Command)(this.client));
+				IBox.addItem(this.boxContents, { name: commandClass.name, value: `${chalk.bold.green('Loaded')}` });
 			}
+		}
+	}
+
+	private async connectPrisma() {
+		try {
+			await prisma.$connect();
+			IBox.addItem(this.boxContents, { name: `${chalk.bold.hex('#16A394')('Prisma Client')}`, value: `${chalk.bold.green('Connected')}` });
+		}
+		catch (error) {
+			console.error(error);
+			IBox.addItem(this.boxContents, { name: `${chalk.bold.hex('#16A394')('Prisma Client')}`, value: `${chalk.bold.red('Error')}` });
 		}
 	}
 }
